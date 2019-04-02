247d7cb8384c80c546fdceb8dfe73eba60940e7a
amalroy <amal.roy@toyota.com>
2019-04-02 10:21:37 -0500

    implementation changes

--- src/com/utd/distributed/process/Process.java
+++ src/com/utd/distributed/process/Process.java
@@ -2,9 +2,9 @@ package com.utd.distributed.process;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import com.utd.distributed.master.Master;
 import com.utd.distributed.util.Message;
@@ -27,21 +27,18 @@ public class Process implements Runnable{
 	private int decision;
 	private boolean decisionDone;
 	
-	private volatile Queue<Message> messageQueue = new LinkedList<>();
+	private volatile BlockingQueue<Message> messageQueue;
 	public Process(int id,Master master,int initialValue,int rounds,int dropCounter,int processCount) {
 		this.id = id;
 		this.master = master;
 		this.maxRound = rounds;
 		this.dropMessageCounter = dropCounter;
 		this.processCount = processCount;
+		this.messageQueue = new LinkedBlockingDeque<>();
 		this.val = new ArrayList<>(Collections.nCopies(processCount, -1));
 		this.level = new ArrayList<>(Collections.nCopies(processCount, 0));
 		this.val.set(this.id, initialValue);
 		this.key = -1;
-		if(id == 0) {
-			Random rand = new Random();
-            this.key = rand.nextInt(maxRound-1) + 1;
-		}
 	}
     
 	public void setProcessNeighbors(Process p[]) {
@@ -55,41 +52,39 @@ public class Process implements Runnable{
 		try {
 			while (!processDone) {
 				if(getMessageFromMaster().equals("Initiate")){
-					//System.out.println("Process "+id+" Initiated");
 					setMessageFromMaster("");
 					master.roundCompletionForProcess(id);	
 				}else 
 	            if (getMessageFromMaster().equals("StartRound")) {
 	            	setMessageFromMaster("");
-	            	//System.out.println("Process "+id+" Entered Round"+round);
 	                if(round < maxRound) {
 	                    if (round == 0 && this.id == 0) {
-	                        Random rand = new Random();
-	                        //this.key = rand.nextInt(maxRound-1) + 1;
-	                        //this.key = 6;
-	                    } else 
-	                    	if(round !=0) {
-	                    		this.processQueue();
-	                    	}
-	                
-	                    round++;
-	                    Message message = new Message(this.key, this.val, this.level);
+	                    	Random rand = new Random();
+	                        key = rand.nextInt(maxRound-1) + 1;
+	                    } else if(round !=0) {
+                    		processQueue();
+                    	}
+	                    Message message = new Message(key, val, level);
+	                    Thread.sleep(1000);
 	                    for (int i = 0,count = 1; i < processCount; i++){
 	                    	if(i!=this.id){
-	                        	Thread.sleep(1000);
 	                            sendMessage(message, i,count++);
 	                        }
 	                    }
-	                    
+	                    setRoundNumber();
 	                }
 	                else {
 	                	processDone = true;
-	                	this.processDecision();
+	                	processDecision();
 	                }
 	                master.roundCompletionForProcess(id);	
 	            }
 	        }
-		} catch (InterruptedException e) {
+		}catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println("Process :"+id+" Round:"+round+" values:"+val.toString()+" Key:"+key+" Levels:"+level.toString());
+		} 
+		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
@@ -102,16 +97,15 @@ public class Process implements Runnable{
 		return messageFromMaster;
 	}
 	
-	
 	public void setMessageFromMaster(String messageFromMaster) {
 		this.messageFromMaster = messageFromMaster;
 	}
 	
 	 public synchronized void sendMessage(Message m,int receiverId,int count){
 		messageNumber = ((round)*(processCount)*(processCount-1))+((id*(processCount-1))+count);
-	    //System.out.println("Message from :"+id+" Receiver :"+receiverId+" Message number :"+messageNumber);
+	    //System.out.println("Message from :"+id+" Receiver :"+receiverId+" Message number :"+messageNumber+" Round no:"+round);
 		if(messageNumber%dropMessageCounter != 0) {
-	        send(m, p[receiverId]);
+			send(m, p[receiverId]);
 	    }
 	    else
 	        System.out.println("Message Number:" + messageNumber +" From process:"+id +" to process:" +receiverId+" was dropped!");
@@ -121,27 +115,41 @@ public class Process implements Runnable{
 		 p.putMessage(m);
 	 }
 	 
-	public void putMessage(Message m){
-		 messageQueue.add(m);
+	public  void putMessage(Message m){
+		if(m == null)
+			System.out.println("Y message is null ?");
+		messageQueue.add(m);
     }
 		
     public synchronized void processQueue() {
        if(!messageQueue.isEmpty()) {
            while (!messageQueue.isEmpty()) {
-               Message m = messageQueue.poll();
-               if(key==-1 && m.key!=-1) {
-                   this.key = m.key;
-               }
-               for (int i = 0; i < processCount; i++) {
-                   if (val.get(i) == -1 && (m.val.get(i)  != -1)) {
-                       val.set(i,m.val.get(i));
-                   }
-                   if (level.get(i) < m.level.get(i)) {
-                       level.set(i, m.level.get(i));
-                   }
-               }
+               Message m;
+               try {
+					m = messageQueue.take();
+	               if(m==null) {
+	            	   //System.out.println("Message null for process:"+id);
+	            	   return;
+	               }else {
+	               if(key==-1 && m.key!=-1) {
+	                   key = m.key;
+	               }
+	               for (int i = 0; i < processCount; i++) {
+	            	   if ( val.get(i) == -1 && (m.val.get(i)  != -1)) {
+	                       val.set(i,m.val.get(i));
+	                   }
+	                   if (level.get(i) < m.level.get(i)) {
+	                       level.set(i, m.level.get(i));
+	                   }
+	               }
+	           }
+           
+		       level.set(id,Collections.min(level)+1);
+		       } catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
            }
-       level.set(id,Collections.min(level)+1);
        }
 	}
     
